9d8284564c5166447a3e88effad036ecc19a017b
TheFaded-Greg-L <gregory.lui23@ycdsbk12.ca>
2022-03-22 16:17:05 -0400

    version 1 up to 5

--- Sketch.java
+++ Sketch.java
@@ -23,15 +23,7 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    /*
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-*/
     draw_section_outlines();
     draw_section1();
     draw_section2();
@@ -76,8 +68,8 @@ public class Sketch extends PApplet {
 
     for(int intRow = 0; intRow < 30; intRow++){
       for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+        intX = 3 + intRow * 300 / 30;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 3 + 300 + intColumn * 300 / 30; //Instead of zero, calculate the proper intY location using 'intColumn'
 
         fill(255);
         noStroke();
@@ -93,6 +85,28 @@ public class Sketch extends PApplet {
    */
   public void draw_section2(){
 
+    int sectTwoX = 0;
+    int sectTwoY = 0;
+
+    for(int sectTwoRow = 0; sectTwoRow < 30; sectTwoRow++){
+      for(int sectTwoColumn = 0; sectTwoColumn < 30; sectTwoColumn++){
+
+        sectTwoX = 3 + 300 + sectTwoRow * 300 / 30;
+        sectTwoY = 3 + 300 + sectTwoColumn * 300 / 30;
+
+        if(sectTwoRow % 2 == 0){
+
+          fill(255);
+          noStroke();
+          rect(sectTwoX, sectTwoY, 5, 5);
+        } else {
+
+          fill(0);
+          noStroke();
+          rect(sectTwoX, sectTwoY, 5, 5);
+        }
+      }
+    }
   }
 
   /**
@@ -101,6 +115,28 @@ public class Sketch extends PApplet {
    */
   public void draw_section3(){
 
+    int sectThreeX = 0;
+    int sectThreeY = 0;
+
+    for(int sectThreeRow = 0; sectThreeRow < 30; sectThreeRow++){
+      for(int sectThreeColumn = 0; sectThreeColumn < 30; sectThreeColumn++){
+
+        sectThreeX = 3 + 600 + sectThreeRow * 300 / 30;
+        sectThreeY = 3 + 300 + sectThreeColumn * 300 / 30;
+
+        if(sectThreeColumn % 2 == 0){
+
+          fill(0);
+          noStroke();
+          rect(sectThreeX, sectThreeY, 5, 5);
+        } else {
+
+          fill(255);
+          noStroke();
+          rect(sectThreeX, sectThreeY, 5, 5);
+        }
+      }
+    }
   }
 
   /**
@@ -108,6 +144,28 @@ public class Sketch extends PApplet {
    */
   public void draw_section4(){
 
+    int sectFourX = 0;
+    int sectFourY = 0;
+
+    for(int sectFourRow = 0; sectFourRow < 30; sectFourRow++){
+      for(int sectFourColumn = 0; sectFourColumn < 30; sectFourColumn++){
+
+        sectFourX = 3 + 900 + sectFourRow * 300 / 30;
+        sectFourY = 3 + 300 + sectFourColumn * 300 / 30;
+
+        if(sectFourColumn % 2 != 0 && sectFourRow % 2 == 0){
+
+          fill(255);
+          noStroke();
+          rect(sectFourX, sectFourY, 5, 5);
+        } else {
+
+          fill(0);
+          noStroke();
+          rect(sectFourX, sectFourY, 5, 5);
+        }
+      }
+    }
   }
 
   /**
@@ -115,6 +173,20 @@ public class Sketch extends PApplet {
    */
   public void draw_section5(){
 
+    int sectFiveX = 0;
+    int sectFiveY = 0;
+
+    for(int sectFiveRow = 30; sectFiveRow > 0; sectFiveRow--){
+      for(int sectFiveColumn = 30; sectFiveColumn > 0; sectFiveColumn--){
+
+        sectFiveX = 3 + sectFiveRow * (300 / sectFiveRow);
+        sectFiveY = 3 + sectFiveColumn * (300 / sectFiveColumn);
+
+        fill(255);
+        noStroke();
+        rect(sectFiveX, sectFiveY, 5, 5);
+      }
+    }
   }
 
   public void draw_section6(){
